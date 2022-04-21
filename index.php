81b86ea43a7873ee47e71e7e9242bde7787f9366
maitemcamboni <maitemendezcamboni@gmail.com>
2022-04-21 13:42:10 -0800

    git commit mensaje

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> 
+    <div>
+    <h1>21 de abril de 2022</h1> <br/>
+    <p>Para concatenar en php usamos "."</p>
+    </div>
+
+    <?php
+
+    $nombre_de_la_variable = "valor";
+    for($i = 0; $i < 10; $i ++){
+        
+        echo "<b>hola mundo</b>";
+        echo "<b>Programando en php</b>" . "<br/>" . "brrrrr";
+    
+    }
+
+   
+    ?>
+
+</body>
+</html>
